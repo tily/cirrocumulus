b0ac5db6886bb10dd31ecf69c18b827f742e17e9
root <root@linwttaa.(none)>
2014-09-20 12:53:53 +0900

    tweak

--- app.rb
+++ app.rb
@@ -70,7 +70,8 @@ get '/cards/:cards/files/:files' do
 
 	if empty_line = params[:empty_line]
 		p 'here'
-		html = @doc.to_s.gsub(/([^>\s])\s*?(<br\s*\/?>\s+?){2,}/) { $1 + "<br /><br />" * empty_line.to_i }
+		#html = @doc.to_s.gsub(/([^>\s])\s*?(<br\s*\/?>\s+?){2,}/) { $1 + "<br /><br />" * empty_line.to_i }
+		html = @doc.to_s.gsub(/((<br\s*\/?>\s+?){2,}/) { "<br /><br />" * empty_line.to_i }
 		p html
 		@doc = Nokogiri::HTML(html)
 	end
